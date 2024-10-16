249ef7feae4b499a0dfc870ffea986386069d753
khadir <tolibxonovm@gmail.com>
2024-10-16 15:02:27 +0500

    imtihon

---
+++ app.js
@@ -0,0 +1,3 @@
+//// Berilgan mahsulotlarni narx yoki sotuv soni bo'yicha tartiblash.
+// //Kirish: Mahsulotlar ro'yxatidagi obyektlarni tanlang va foydalanuvchi
+// //belgilagan tartib bo'yicha saralang (asc yoki desc ).
